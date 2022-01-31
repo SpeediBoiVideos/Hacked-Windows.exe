202e3e57941fcb1c46e6f7c9ac96419c1d46dbc8
SpeediBoiVideos <96069681+SpeediBoiVideos@users.noreply.github.com>
2022-01-31 20:49:43 +0530

    Add files via upload

---
+++ Hacked WIndows/Hacked Windows/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace HackedWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.ApplicationExitTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(0, 0);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(800, 450);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            this.Player.Enter += new System.EventHandler(this.Player_Enter);
+            // 
+            // ApplicationExitTimer
+            // 
+            this.ApplicationExitTimer.Interval = 20000;
+            this.ApplicationExitTimer.Tick += new System.EventHandler(this.ApplicationExitTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Player);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.Timer ApplicationExitTimer;
+    }
+}
+
---
+++ Hacked WIndows/Hacked Windows/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace HackedWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string message = "Created by SpeediBoi. Coded with C#";
+            string title = "Hacked Windows";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+            InitializeComponent();
+            Player.uiMode = "none";
+            this.TopMost = true;
+            int w = Screen.PrimaryScreen.Bounds.Width;
+            int h = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(w, h);
+            Player.settings.volume = 100;
+            Player.TabStop = false;
+            checking();
+        }
+
+        private async void checking()
+        {
+        Check:
+            if (Player.fullScreen == true)
+            {
+                Player.fullScreen = false;
+            }
+            await Task.Delay(200);
+            goto Check;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.play();
+            ApplicationExitTimer.Enabled = true;
+        }
+
+
+
+        private void ApplicationExitTimer_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Player_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
